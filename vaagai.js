3b1ab09bb6344c33911a9390be25308d88a868d7
Veeraragavan <70055617+Veeraragavan-k@users.noreply.github.com>
2024-11-27 12:24:57 +0530

    Add files via upload

---
+++ vaagai.js
@@ -0,0 +1,10 @@
+
+
+
+  const menuToggle = document.querySelector('.menu-toggle');
+  const menuBar = document.querySelector('.menu-bar');
+
+  menuToggle.addEventListener('click', () => {
+    menuBar.classList.toggle('active');
+  });
+
